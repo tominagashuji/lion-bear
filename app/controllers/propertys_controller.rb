eb88ad66b113e8e0e8e57180bd6c891d0f8a3689
冨永修司 <tominagashuuji@shuji.local>
2019-05-10 18:38:18 +0900

    物件情報のpropertys コントローラー作成

---
+++ app/controllers/propertys_controller.rb
@@ -0,0 +1,13 @@
+class PropertysController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'propertys/index'
+
+  get 'propertys/new'
+
+  get 'propertys/show'
+
+  get 'propertys/destroy'
+
 end
