948cee3ce4c50883ae2e7f2cb6491ff8c26f7410
冨永修司 <tominagashuuji@shuji.local>
2019-05-15 21:07:02 +0900

    物件登録動作確認済み

--- app/controllers/propertys_controller.rb
+++ app/controllers/propertys_controller.rb
@@ -7,6 +7,16 @@ class PropertysController < ApplicationController
     @property = Property.new
   end
 
+  def create
+    @property = Property.create(property_params)
+
+    if @property.save
+      redirect_to propertys_path, notice: '物件登録に成功しました！'
+    else
+      render 'new'
+    end
+  end
+
   def show
   end
 
@@ -15,4 +25,11 @@ class PropertysController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :rent, :address, :age, :remarks)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'propertys#index'
-  resources :propertys, only: [:index,:new,:show,:destroy,:edit]
+  # resources :propertys, only: [:index,:new,:show,:destroy,:edit]
+  resources :propertys
 end
